bb5a678c06e7a01bc8f91fa1dc3be20dcf04a3ae
Kang Shend Rong <shendrongddv@gmail.com>
2024-06-04 14:52:18 +0700

    first commit

---
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -0,0 +1,22 @@
+export default function Dashboard() {
+  return (
+    <div className="space-y-4 p-6">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+        {/* <InfoCard
+        icon={Clock}
+        label="In Progress"
+        numberOfItems={coursesInProgress.length}
+     /> */}
+        {/* <InfoCard
+        icon={CheckCircle}
+        label="Completed"
+        numberOfItems={completedCourses.length}
+        variant="success"
+     /> */}
+      </div>
+      {/* <CoursesList
+      items={[...coursesInProgress, ...completedCourses]}
+    /> */}
+    </div>
+  );
+}
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <section className="container">
-      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-        Native
-      </button>
-    </section>
-  );
-}
